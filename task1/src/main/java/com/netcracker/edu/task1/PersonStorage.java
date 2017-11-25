99e3318e790ae4ea540382df27d9e435e24ce93a
Nikita Nagorny <nikiton-n@mail.ru>
2017-11-25 19:28:13 +0300

    task1: Add sorting to PersonStorage
    Signed-off-by: Nikita Nagorny <nikiton-n@mail.ru>

--- task1/src/main/java/com/netcracker/edu/task1/PersonStorage.java
+++ task1/src/main/java/com/netcracker/edu/task1/PersonStorage.java
@@ -1,15 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.netcracker.edu.task1;
 
 import java.util.Arrays;
-/**
- *
- * @author PC-Administrator
- */
+import java.util.Comparator;
+
 public class PersonStorage {
     
     private Person[] arr;
@@ -57,6 +50,78 @@ public class PersonStorage {
         }
         return arr[id];
     }
+    
+    private void sort(Comparator<Person> comparator) {
+        Person temp;
+        for (int i = 0; i < tail; i++) {
+        for (int j = 1; j < (tail - i); j++) {
+            if (comparator.compare(arr[j - 1], arr[j]) > 0) {
+                temp = arr[j - 1];
+                arr[j - 1] = arr[j];
+                arr[j] = temp;
+            }
+        }
+        }
+    }
+    
+    public void sortByAge() {
+        sort(new SortedByAge() {
+            
+            @Override
+            public int compare(Person obj1, Person obj2) {
+            
+             int age1 = obj1.getAge();
+             int age2 = obj2.getAge();
+
+             if(age1 > age2) {
+                    return 1;
+             }
+             else if(age1 < age2) {
+                    return -1;
+             }
+             else {
+                    return 0;
+             }
+       }
+        });
+    }
+    
+    public void sortById() {
+        sort(new SortedById() {
+            
+            @Override
+            public int compare(Person obj1, Person obj2) {
+            
+             int id1 = obj1.getId();
+             int id2 = obj2.getId();
+
+             if(id1 > id2) {
+                    return 1;
+             }
+             else if(id1 < id2) {
+                    return -1;
+             }
+             else {
+                    return 0;
+             }
+       }
+        });
+    }
+    
+    public void sortBySurname() {
+        sort(new SortedBySurname() {
+
+            @Override
+            public int compare(Person obj1, Person obj2) {
+            
+            String str1 = obj1.getSurname();
+            String str2 = obj2.getSurname();
+            
+            return str1.compareTo(str2);
+            }
+        });
+    }
+    
     public void print() {
         for (Person i : arr) {
             System.out.println("ID: " + i.getId() + "; Surname: " + i.getSurname() +
